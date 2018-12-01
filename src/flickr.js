a03021a8a60a7b524e6cee71f075ae52c7210d69
dondenoncourt <dondenoncourt@gmail.com>
2018-12-01 11:25:36 -0500

    images now render

--- src/flickr.js
+++ src/flickr.js
@@ -1,26 +1,45 @@
-//const { flickrSearch } = require("./flickr")
-const { flickr } = require("./model")
+//const { flickr } = require("./model")
 const React = require('react')
 const createReactClass = require('create-react-class');
 
 module.exports = createReactClass({
   displayName: 'Flickr',
 
-  getInitialState() { return {term: ""} },
+  getInitialState() { return {term: "", images: []} },
 
   termChanged({currentTarget: t}) { this.setState({term: t.value}) },
 
-  //searchClicked(_) { flickrSearch(this.state.term).fork(this.props.showError, (x) => console.log(x) ) },
-  //searchClicked(_) { flickrSearch(this.state.term).fork((x) => console.log(x), (x) => console.log(x) ) },
-  searchClicked(_) { flickr(this.state.term) },
-  //searchClicked(_) { console.log(flickr) },
+  updateResults(xs) { console.log(xs); this.setState({results: xs}) },
+
+  //searchClicked(_) { flickr(this.state.term).fork(this.props.showError, this.updateResults) },
+  //searchClicked(_) { console.log('searchClicke(): '+flickr(this.state.term)) },
+  searchClicked(_) {
+    let u = new URLSearchParams();
+    u.append('method', 'flickr.photos.search');
+    u.append('api_key', 'c45ce720ddd5058e972b9221a79cbd01');
+    u.append('format', 'json');
+    u.append('nojsoncallback', '1');
+    u.append('tags', this.state.term);
+    u.append('extras', 'url_s');
+    const apiCall = fetch('https://api.flickr.com/services/rest?' + u);
+    apiCall.then((response)=> { return response.json() })
+    .then((data) => { return data.photos.photo.map((photo)=> photo.url_s) })
+    .then((urls) => {
+      let imgs = []
+      let url_s = urls.map(url => `<img src='${url}'></img>`)
+      url_s.forEach((url) => imgs.push(url))
+      this.setState({images:  imgs })
+    })
+    .catch(e => {console.log('oh poop...:'+e) })
+  },
 
   render() {
+    //const images = this.state.images.map(src => <img src={src} key={src} ></img>)
     return (
       <div id="flickr">
         <input onChange={this.termChanged} />
         <button onClick={this.searchClicked}>Search</button>
-        <div id="results"></div>
+        <div dangerouslySetInnerHTML={{ __html: this.state.images }} />
       </div>
     );
   }
--- src/model.js
+++ src/model.js
@@ -1,31 +1,22 @@
-//const { getJSON } = require('jquery')
-const { compose } = require('rambda')
-//const fetch = require('whatwg-fetch').fetch
 
+/*
 let urls = data => {
   const url_s = data.photos.photo.map((photo)=> photo.url_s)
   return url_s
 }
-let images = (urls)=> {
-  //console.log("urls:"+urls)
-  return urls.map((url) => ('<img />', { src: url })[0] )
-}
-let responseToImages = compose(images, urls)
-
-let img = url => $('<img />', { src: url }
-
+//let Images = urls => urls.map(url => ('<img />', { src: url }))
+//let responseToImages = compose(Images, urls)
 let flickr = (tags)=> {
+  let url_s = {}
   //let url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=c45ce720ddd5058e972b9221a79cbd01&tags='+tags+'&extras=url_s&format=json&jsoncallback=?'
   //see: https://hacks.mozilla.org/2015/03/this-api-is-so-fetching/
   // test API at https://www.flickr.com/services/api/explore/flickr.photos.search
   // https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=338af50560c37293e53adcee58fa5d84&tags=elephant&extras=url_s&format=rest&api_sig=fe60306659c77c3876181cdd6f494619
   let u = new URLSearchParams();
-  //u.append('method', 'flickr.interestingness.getList');
   u.append('method', 'flickr.photos.search');
   u.append('api_key', 'c45ce720ddd5058e972b9221a79cbd01');
   u.append('format', 'json');
   u.append('nojsoncallback', '1');
-  debugger
   u.append('tags', tags);
   u.append('extras', 'url_s');
   const apiCall = fetch('https://api.flickr.com/services/rest?' + u);
@@ -35,12 +26,14 @@ let flickr = (tags)=> {
     //console.log(json)
     return json
   })
-  .then(responseToImages).catch(e => {console.log('on poop...:'+e) })
+  //.then(responseToImages)
+  .then((data) => {
+    //urls
+    url_s = data.photos.photo.map((photo)=> photo.url_s)
+  })
+  .catch(e => {console.log('oh poop...:'+e) })
+  return url_s
 }
+*/
 
-//export default {
-  //_responseToImages: responseToImages,
-  //flickr: flickr,
-//}
-
-module.exports = { flickr, responseToImages }
+//module.exports = { flickr }
